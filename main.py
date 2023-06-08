ee2c302560a306c7393170dfbe37c0bc724013e8
Iori Mizutani <iori.mizutani@gmail.com>
2023-06-08 23:02:05 +0200

    chore: add docker-ros2-xarm

--- main.py
+++ main.py
@@ -1,291 +1,200 @@
-#!/usr/bin/env python
-
-#Modules for ROS
-import rospy, rosservice
-from xarm_msgs.srv import SetInt16, Move, GripperConfig, GripperMove, SetLoad, ClearErr
-from xarm_msgs.msg import RobotMsg
-from time import sleep
-
-#Modules for Classifier
-import joblib
-import numpy as np
-
-#Modules for Socket connection
-from socket import socket, timeout, AF_INET, SOCK_STREAM
-from sys import stdout
-
-#for input
-import keyboard
-from playsound import playsound
+#!/usr/bin/env python3
+import aenum
+import logging
 
 
-#Global Variables to keep track of the gripper state,the robot mode, delete position and finish teaching
-gripper_state=1
-robot_mode = 0
-delete_position = 0
-finish = 0
-
-#Clear Error on the xArm
-def clear_error():
-    rospy.wait_for_service('/xarm/clear_err')
-    clear_err = rospy.ServiceProxy('/xarm/clear_err', ClearErr)
-
-    print clear_err()
-
-#xArm Mode and State Functions
-
-def standard_mode():
-    set_mode_client(0)
-    global robot_mode
-    robot_mode = 0
-    #feedack
-    print 'Xarm in Standard Mode'
-    playsound ('assets/standard_mode.mp3')
+class Mode(aenum.Enum):
+    __slot__ = "name"
+    STANDARD_MODE = 0
+    TEACH_MODE = 1
 
 
-def teach_mode():
-    set_mode_client(2)
-    global robot_mode
-    robot_mode = 1
-    #feedback
-    print 'Xarm in Teach Mode'
-    playsound('assets/teach_mode.mp3')
-
-
-def set_mode_client(new_mode):
-    rospy.wait_for_service('/xarm/set_mode')
-    set_mode = rospy.ServiceProxy('/xarm/set_mode', SetInt16)
-
-    rospy.wait_for_service('/xarm/set_state')
-    set_state = rospy.ServiceProxy('/xarm/set_state', SetInt16)
-
-    print set_mode(new_mode)
-    print set_state(0)
-
-#xArm move Functions
-def return_home():
-
-    global robot_mode
-    if robot_mode != 0:
-        standard_mode()
-    #Modified home position for gripper
-    rospy.wait_for_service('/xarm/move_joint')
-    go_home = rospy.ServiceProxy('/xarm/move_joint', Move)
-
-    responseGoHome = go_home([0,0,0,0,0,-1.562,0], 0.7, 7, 0, 0)
-    print responseGoHome
-
-
-def move_joints(position):
-
-    global robot_mode
-    if robot_mode != 0:
-        standard_mode()
-    rospy.wait_for_service('/xarm/move_joint')
-    move_joint = rospy.ServiceProxy('/xarm/move_joint', Move)
-
-    responseMoveJoint = move_joint(position, 0.7, 7, 0, 0)
-    print responseMoveJoint
-
-def move_sequence(positions):
-    return_home()
-    home_gripper()
-
-    for j in range(3):
-        for i in range(len(positions)):
-            sleep(0.5)
-            move_joints(positions[i])
-            gripper_action(gripper_states[i])
-    sleep(0.5)
-    return_home()
-    home_gripper()
-
-#Confirm Function and delete position
-def confirm_pos():
-    statenow = rospy.wait_for_message("/xarm/xarm_states", RobotMsg)
-    state = statenow.angle
-    positions.append(state)
-    global gripper_state
-    gripper_states.append(gripper_state)
-    #feedback
-    print 'Position confirmed'
-    playsound('assets/position_confirmed.mp3')
-
-#delete the last confirmed position
-def delete_pos():
-    if len(positions) > 0:
-        del positions[-1]
-        del gripper_states[-1]
-        #feedback
-        print "Position deleted"
-        playsound('assets/position_deleted.mp3')
-
-#recognize keyboard input to delete a position or to break the loop and finish training
-def key_input(key):
-    print key.name
-    if key.name == 'shift':
-        global delete_position
-        delete_position = 1
-    if key.name == 'enter':
-        global finish
-        finish = 1
-
-#Gripper Functions
-
-def set_load():
-    rospy.wait_for_service('/xarm/set_load')
-    setload = rospy.ServiceProxy('/xarm/set_load', SetLoad)
-    responseSetLoad = setload(0.82,0,0,48)
-    print responseSetLoad
-
-def config_gripper(speed):
-    rospy.wait_for_service('/xarm/gripper_config')
-    gripper_config = rospy.ServiceProxy('/xarm/gripper_config', GripperConfig)
-    responseGripperConfig = gripper_config(speed)
-    print responseGripperConfig
-
-def move_gripper(position):
-    rospy.wait_for_service('/xarm/gripper_move')
-    gripper_move = rospy.ServiceProxy('/xarm/gripper_move', GripperMove)
-    responseGripperMove = gripper_move(position)
-    print responseGripperMove
-
-def open_gripper():
-    move_gripper(850)
-    global gripper_state
-    gripper_state = 0
-
-def close_gripper():
-    move_gripper(620)
-    global gripper_state
-    gripper_state = 1
-
-def home_gripper():
-    move_gripper(0)
-
-def gripper_action(x):
-    if (x == 0):
-        open_gripper()
-    elif (x == 1):
-        close_gripper()
-
-
-#Function to receive EMG Signals (by Iori)
-def receive_emg_data(sock):
-    data = bytes()
-    try:
-        while b"\r\n" not in data:
-            chunk = sock.recv(1) # receive byte-by-byte
-            if not chunk:
-                break
-            else:
-                data += chunk
-    except timeout:
+# Global Variables to keep track of the gripper state,the robot mode, delete position and finish teaching
+class RosObject:
+    def __init__(self):
+        # rospy.init_node('listener', anonymous=True)
+        # rospy.set_param('/xarm/wait_for_finish', True)
         pass
-    d = data.split()
-    if len(d) == 18:
-        d1 = " ".join([str(int(i)) for i in d[1:17]])
-        arr = [int(s) for s in d1.split(' ')]
-    return arr
+
+    def clear(self):
+        # rospy.wait_for_service('/xarm/clear_err')
+        # err = rospy.ServiceProxy('/xarm/clear_err', ClearErr)
+        # if err:
+        #    logging.debug(err)
+        pass
 
 
+class XArm7(RosObject):
+    def __init__(self):
+        super().__init__()
+        # TODO
+        self.gripper = Gripper()
+        self.gripper.set_load()
+        self.gripper.set_speed(1500)
+        self.current_mode = None
+        self.set_mode(Mode.STANDARD_MODE)
+        self.trajectory = []
 
-if __name__ == '__main__':
+    def set_mode(self, mode):
+        """
+        set_mode(mode: MODE)
+        mode:
+            - 0: STANDARD_MODE
+            - 2: TEACH_MODE
+        """
+        self.current_mode = mode
+        # TODO: convert to aac
+        # playsound ('assets/standard_mode.mp3')
+        # playsound('assets/teach_mode.mp3')
 
-    #Set up connection to Myo EMG Stream
-    MYO_APP_IP = '10.2.1.31'
-    MYO_APP_PORT = 5678
-    s = socket(AF_INET, SOCK_STREAM)
-    s.connect((MYO_APP_IP, MYO_APP_PORT))
+        # rospy.wait_for_service('/xarm/set_mode')
 
-    #Load the classifier
-    knn = joblib.load('Classifier.pkl')
+        # set_mode_client(2) ??
 
-    #Initialize ROS node and set params
-    rospy.init_node('listener', anonymous=True)
-    rospy.set_param('/xarm/wait_for_finish', True)
+        # set_mode = rospy.ServiceProxy('/xarm/set_mode', SetInt16)
 
-    #Clear Errors
-    clear_error()
+        # rospy.wait_for_service('/xarm/set_state')
+        # set_state = rospy.ServiceProxy('/xarm/set_state', SetInt16)
 
-    #Empty lists to save the positions and gripper states in
-    positions = []
-    gripper_states = []
-    #Control variable so that a function is not executed twice in a row
-    cont = 0
-    #Control variables so that a position can only be confirmed if position or gripper state has been changed
-    taught = 0
-    gripped = 0
+        # set_mode(mode)
+        # set_state(0)
+        logging.info(f"{self.name} in {self.mode.name}")  # pyright: ignore
 
-    #Set TCP Payload for the Gripper
-    set_load()
+    def home(self):
+        if self.current_mode != Mode.STANDARD_MODE:
+            self.set_mode(Mode.STANDARD_MODE)
 
-    #Enable amd configurate Gripper
-    config_gripper(1500)
+        # Modified home position for gripper
+        # rospy.wait_for_service('/xarm/move_joint')
+        # go_home = rospy.ServiceProxy('/xarm/move_joint', Move)
+
+        # responseGoHome = go_home([0,0,0,0,0,-1.562,0], 0.7, 7, 0, 0)
+        # responseGoHome()
+
+    def joint(self, position):
+        if self.current_mode != Mode.STANDARD_MODE:
+            self.set_mode(Mode.STANDARD_MODE)
+
+        # rospy.wait_for_service('/xarm/move_joint')
+        # move_joint = rospy.ServiceProxy('/xarm/move_joint', Move)
+
+        # responseMoveJoint = move_joint(position, 0.7, 7, 0, 0)
+        # responseMoveJoint()
+
+    def execute(self):
+        self.home()
+        # self.set_gripper(GripperState.HOME)
+
+        # for j in range(3):
+        #    for i in range(len(positions)):
+        #        sleep(0.5)
+        #        move_joints(positions[i])
+        #        gripper_action(gripper_states[i])
+        # sleep(0.5)
+        # return_home()
+        # home_gripper()
+
+    # confirm Function and delete position
+    def record(self):
+        # statenow = rospy.wait_for_message("/xarm/xarm_states", RobotMsg)
+        # state = statenow.angle
+        # self.positions.append(state)
+        # self.gripper_state.append(gripper_state)
+        # feedback
+        # print 'Position confirmed'
+        # playsound('assets/position_confirmed.mp3')
+        pass
+
+    # delete the last confirmed position
+    def undo(self):
+        """
+        if len(positions) > 0:
+            del positions[-1]
+            del gripper_states[-1]
+            #feedback
+            print "Position deleted"
+            playsound('assets/position_deleted.mp3')
+        """
+        pass
 
 
-    #Set robot to standard mode
-    standard_mode()
+class Gripper(RosObject):
+    def __init__(self):
+        pass
 
-    #3 consecutive predictions (for teach and standard mode onlz 2/3 have to be correct)
-    pred = [0,0,0]
+    def move(self, position):
+        """
+        move
+        position:
+            - 0: reset
+            - 620: grab
+            - 850: release
+        """
+        # rospy.wait_for_service('/xarm/gripper_move')
+        # gripper_move = rospy.ServiceProxy('/xarm/gripper_move', GripperMove)
+        # responseGripperMove = gripper_move(position)
+        # responseGripperMove()
+        pass
 
-    #activate keyboard input
-    keyboard.on_press(key_input)
+    def set_load(self):
+        # rospy.wait_for_service('/xarm/set_load')
+        # setload = rospy.ServiceProxy('/xarm/set_load', SetLoad)
+        # responseSetLoad = setload(0.82,0,0,48)
+        # responseSetLoad()
+        pass
 
-    #This is the main loop features are received and classified
+    def set_speed(self, speed):
+        # rospy.wait_for_service('/xarm/gripper_config')
+        # gripper_config = rospy.ServiceProxy('/xarm/gripper_config', GripperConfig)
+        # responseGripperConfig = gripper_config(speed)
+        # responseGripperConfig()
+        pass
+
+    def toggle(self):
+        pass
+
+
+class Gesture(aenum.Enum):
+    zero = 0
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+
+
+if __name__ == "__main__":
+    xarm7 = XArm7()
+    pred = Gesture.zero
+
+    def update():
+        return None
+
     while True:
+        # Standard Mode
+        if pred == Gesture.zero:
+            xarm7.set_mode(Mode.STANDARD_MODE)
 
-        for j in range(3):
-            arr = receive_emg_data(s)
-            datanew = np.array(arr)
-            data = datanew.reshape(1, -1)
-            pred[j] = knn.predict(data)[0]
+        # Teach Mode
+        elif pred == Gesture.one:
+            xarm7.set_mode(Mode.TEACH_MODE)
 
+        # Confirm Position
+        elif pred == Gesture.two:
+            xarm7.record()
 
+        # Toggle Gripper
+        elif pred == Gesture.three:
+            xarm7.gripper.toggle()
 
-        #Standard Mode
-        if (pred[0] == 0 and (pred[0]==pred[1] or pred[0]==pred[2]) and robot_mode != 0):
-            cont = 0
-            standard_mode()
+        # Delete the last confirmed position
+        elif pred == Gesture.four:
+            xarm7.undo()
 
-        #Teach mode
-        elif (pred[0] == 1 and (pred[0]==pred[1] or pred[0]==pred[2]) and robot_mode != 1):
-            cont = 1
-            taught = 1
-            teach_mode()
-
-       #Confirm Position
-        elif (pred[0] == 2 and pred[0]==pred[1]==pred[2] and pred[0] != cont and robot_mode == 0 and (taught == 1 or gripped == 1)):
-            cont = 2
-            taught = 0
-            gripped = 0
-            confirm_pos()
-            #I had this in there to disable two sequential confirmations, but it is redundant now, and taking it out would prob. improve the response time
-            sleep(1)
-
-        #Open or Close Gripper
-        elif (pred[0] == 3 and pred[0]==pred[1]==pred[2] and pred[0] != cont and robot_mode == 0):
-            cont = 3
-            gripped = 1
-            if (gripper_state == 1):
-                open_gripper()
-            elif (gripper_state == 0):
-                close_gripper()
-
-        #Delete the last confirmed position
-        elif (delete_position == 1):
-            delete_pos()
-            delete_position = 0
-
-
-        #Finish Teaching
-        elif (finish == 1):
+        # Finish Teaching
+        elif pred is None:
             break
 
-        else:
-            pass
+        pred = update()
 
-    print "Sequence will now be executed"
-    move_sequence(positions)
-
+    logging.info("executing the recorded sequence")
+    xarm7.execute()
